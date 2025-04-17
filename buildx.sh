2ac6374c85a7602d9549579373c2f07e0864b904
Fredrik Gjellestad <freddyg91@gmail.com>
2025-04-17 13:40:57 +0200

    added my buildx script

---
+++ buildx.sh
@@ -0,0 +1,15 @@
+echo "enter version number for calibre-web-chat docker image"
+echo "last version = 0.2.1"
+read VERSION
+script_path="$0"
+full_path="$(cd "$(dirname "$script_path")" && pwd)/$(basename "$script_path")"
+echo $full_path
+echo "you have selected $VERSION"
+if [ "$VERSION" = "development" ]; then
+    echo "tagging build with $VERSION"
+    docker buildx build --platform linux/amd64,linux/arm64  -t enzanto/calibre-web-chat:$VERSION --push .
+else
+    echo "tagging build with latest,$VERSION"
+    sed -i "0,/=/{s/=.*/= $VERSION\"/}" $full_path
+    docker buildx build --platform linux/amd64,linux/arm64 -t enzanto/calibre-web-chat -t enzanto/calibre-web-chat:$VERSION --push .
+fi
